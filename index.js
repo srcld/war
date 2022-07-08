f68af7ac86308516a0dac3e206e50ef4831970d3
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2022-07-08 15:30:33 +0200

    quick fix META-INF order by adding waiting step Version bump to 0.0.3

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const fs = require('fs');
 const archiver = require('archiver');
 
-// script basically
-// copied from https://www.npmjs.com/package/archiver
-
 const createFolderIfNotPresent = function (dir) {
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 }
 
 const createManifest = function (array = [], dir = './META-INF', name = 'MANIFEST.MF') {
+    createFolderIfNotPresent(dir);
+
     const data = array.map((o) => {
         return o.key + ': ' + o.value;
     }).join('\n');
-    createFolderIfNotPresent(dir);
+
     fs.writeFileSync([dir, name].join('/'), data);
 }
+
+const logLine = function (num = 25) {
+    console.log('-'.repeat(num));
+};
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const createArchive = function (name, folder, sources = [], format = 'zip', ending = '.war') {
     const path = './' + folder ? (folder.endsWith('/') ? folder : (folder + '/')) : '';
     createFolderIfNotPresent(path);
-    const output = fs.createWriteStream(path + name + ending);
-    const archive = archiver('zip', {});
-    return new Promise((resolve) => {
-        output.on('close', function () {
+    const fileName = name + ending,
+        fullPath = path + fileName,
+        output = fs.createWriteStream(fullPath),
+        archive = archiver(format, {});
+    return new Promise(async (resolve) => {
+        // check quickstart -> https://www.npmjs.com/package/archiver
+        output.on('close', () => {
             console.log(archive.pointer() + ' total bytes');
-            console.log('archiver has been finalized and the output file descriptor has closed.');
-            resolve(true);
+            resolve({fullPath, fileName, path});
         });
 
         output.on('end', function () {
@@ -35,8 +40,7 @@ const createArchive = function (name, folder, sources = [], format = 'zip', endi
         });
 
         archive.on('warning', function (err) {
-            if (err.code === 'ENOENT') {
-                // log warning
+            if (err.code === 'ENOENT') { // log warning
             } else {
                 resolve(false); // throw err;
             }
@@ -48,25 +52,51 @@ const createArchive = function (name, folder, sources = [], format = 'zip', endi
 
         archive.pipe(output);
 
-        sources.map((sourceCfg) => {
-            archive.directory(sourceCfg.source, sourceCfg.target);
-        });
 
-        archive.finalize();
+        for (let sourceCfg of sources) {
+            let {source, target} = sourceCfg || {};
+            if (typeof source !== 'undefined' && typeof target !== 'undefined') {
+                console.log('ADDING: ' + source + ' : ' + target);
+                archive.directory(source, target);
+                await wait(20)
+
+                // to ensure the order, we delay here ... not good but working
+                // otherwise ie karaf fileinstall will complain
+
+                // can be checked use jar tool
+                // jar tf build/generatedJarWar.war
+
+                // expected output starting like:
+                // META-INF/
+                // META-INF/MANIFEST.MF
+
+            }
+        }
+
+        await archive.finalize();
     });
 
 }
 
 module.exports = {
-    createManifest,
-    createArchive,
+    buildByConfig: function (cfg = {}) {
+        const {name, targetFolder, sources, manifestArray} = cfg;
+        return this.build(name, targetFolder, sources, manifestArray);
+    },
     build: function (name, targetFolder, sources, manifestArray = []) {
+        logLine();
         if (manifestArray.length) {
             console.log('CREATING MANIFEST')
             createManifest(manifestArray);
         }
         console.log('CREATING ARCHIVE')
-        createArchive(name, targetFolder, sources);
-        console.log('DONE.')
-    }
+        logLine();
+        return createArchive(name, targetFolder, sources).then((success) => {
+            logLine();
+            console.log(success ? 'DONE.' : 'BUILD FAILED.')
+            return success;
+        });
+    },
+    createManifest,
+    createArchive
 }
